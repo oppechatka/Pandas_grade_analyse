7f342ae0e774447de50c10af4b3ba6a7190a5c41
hinahin <hinahin.nightchild@gmail.com>
2020-04-01 19:01:51 +0500

    First commit

--- analyse.py
+++ analyse.py
@@ -1,3 +1,14 @@
 import pandas as pnd
 
-print("Hello Pandas")
+TEST_FILE_NAME = "KSE.xlsx"
+
+data = pnd.read_excel(TEST_FILE_NAME)
+
+
+print(data[["Код студента", "Фамилия", "Имя"]][data["Фамилия"] == 'Васильева'])
+
+
+for x, y in data.iterrows():
+	print(x, y)
+	break
+
