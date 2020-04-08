1ce4f54e83209346c13df031470bef711d75f15f
hinahin <hinahin.nightchild@gmail.com>
2020-04-08 22:23:06 +0500

    Selenium подключен

--- test.py
+++ test.py
@@ -1,22 +1,13 @@
-from os import listdir
-import pandas as pnd
-
-DIRECTORY = './grade_reports/full_grade_name/'
-
-file_list = listdir(DIRECTORY)
-dict_file = dict()
-
-for x in range(len(file_list)):
-	file = file_list[x].split(sep="_")
-	file = file[1:-3]
-	if file[-1] == "net":
-		key_str = '_'.join(file[:-3]) + '_' + ''.join(file[-3:])
-		dict_file[key_str] = file_list[x]
-	else:
-		key_str = '_'.join(file[:-2]) + '_' + ''.join(file[-2:])
-		dict_file[key_str] = file_list[x]
+import requests
+from selenium import webdriver
 
 
-print(dict_file)
+driver = webdriver.Firefox()
+driver.close()
 
-# print(new_list.index('Grade'), new_list.index('Cohort Name'))
+# with requests.Session() as s:
+#     r = s.get(url, headers=headers)
+#     r1 = s.post(url, headers=headers)
+#     if 'csrftoken' in s.cookies:
+#         login_data['csrfmiddlewaretoken'] = r.cookies['csrftoken']
+#     print(s.post(url, data=login_data))
