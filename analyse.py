e6d9ea4aa92d631384ee037fd0460779aeda5b49
oppa <o.obaskalova@yandex.ru>
2022-02-22 14:08:13 +0500

    Исправления v_2_ 22_02_2022, 18:01

--- analyse.py
+++ analyse.py
@@ -141,9 +141,16 @@ def get_report_settings(request_file: str, statement_type: str):
         grade_report_df = pnd.read_csv(gs.GRADE_REPORTS_DIRECTORY + '/' + grade_report_file)  # DF файла выгрузки
         columns_list = get_columns(grade_report_df.columns.tolist())  # получаем список всех столбцов с оценками
 
-        grade_settings = {"Columns_for_order": columns_list,  # создаем словарь настроек для всех столбцов
-                          "Columns_for_report": columns_list,
-                          }
+        crs_request_df = pnd.read_excel(gs.REQUESTS_DIRECTORY + '/' + request_file, 0)  # Берем первый лист заявки
+        grade_cipher = "_".join(crs_request_df.iloc[9, 1].split(sep='_')[:-1]).casefold()  # Берем шифр курса
+        try:
+            grade_settings = {"Columns_for_order": columns_list,  # создаем словарь настроек для всех столбцов
+                              "Columns_for_report": columns_list,
+                              "Sheet_name": gs.courses[grade_cipher]["Sheet_name"],
+                              }
+        except KeyError:
+            logger.error(f'Нет словаря с настройками для курса {grade_cipher.upper()} в grade_settings.py')
+            return 0  # обработка отсутствия словаря с настройками
 
         return grade_settings
 
@@ -497,6 +504,53 @@ def search_by_fio(file: str):
     df_full.to_excel(f'{gs.STATEMENTS_DIRECTORY}/{session_course}_fio.xlsx', index=False)
     logger.info(f'{session_course}_fio.xlsx: найдено {len(df_full.index)}')
 
+def create_requests(file: str):
+    """
+    Функция по почтам и шифрам запуска курса формирует файлы запроса.
+    Требуется наличие файла tmp.xlsx папкой выше с шаблоном первого листа файла запроса
+    """
+    df_template = pnd.read_excel('tmp.xlsx', sheet_name=0)
+    try:
+        df_list = pnd.read_excel(gs.REQUESTS_DIRECTORY + '/' + file, sheet_name=0)
+    except:
+        logger.error(f'Нет файла запроса {file}')
+        return -1
+    df_list = df_list.sort_values(['session'], ascending=[False])
+    df_list.rename(columns={'upn (e-mail)': 'Email'}, inplace=True)
+    df_list['Email'] = df_list['Email'].str.lower()
+    for i in df_list.groupby('session'):
+        df_tmp = i[1]
+        try:
+            grade_report_file = get_report_list(gs.GRADE_REPORTS_DIRECTORY)[i[0]]  # файл выгрузки
+        except KeyError:
+            logger.error(f'Нет файла Grade_Report для запроса {i[0]}')
+            return -2
+        try:
+            df_grade = pnd.read_csv(gs.GRADE_REPORTS_DIRECTORY + '/' + grade_report_file, sep=',')
+        except:
+            logger.error(f'Нет файла отчета {grade_report_file}')
+            return -3
+
+        # слияние
+        df_grade = df_grade.loc[df_grade['Email'].isin(df_tmp['Email'])]
+        # df_grade['fio'] = df_grade['Last Name'] + ' ' + df_grade['First Name'] + ' ' + df_grade['Second Name']
+        # df_tmp1 = df_grade.sort_values(['Email'], ascending=[False])
+        # df_full = pnd.merge(df_tmp, df_grade[
+        #     ['fio', 'Student ID', 'Email', 'Username', 'Last Name', 'First Name', 'Second Name', 'Enrollment Status', 'Grade percent']],
+        #                     on='Email',
+        #                     how='left')
+        df_full = df_tmp
+        df_full.rename(columns={'Email': 'Адрес электронной почты'}, inplace=True)
+        # print(df_full.head(5))
+        df_template.iloc[9,1] = i[0]
+        writer = pnd.ExcelWriter(f'{gs.REQUESTS_DIRECTORY}/{i[0]}_req.xlsx', engine='xlsxwriter')
+        df_template.to_excel(writer, sheet_name='main', index=False)
+        df_full.to_excel(writer, sheet_name='list', index=False)
+        writer.save()
+        writer.close()
+        logger.info(f'{i[0]}_req.xlsx: найдено {len(df_full.index)}')
+
+
 
 if __name__ == "__main__":
     for file in gs.REQUESTS_FILES:
@@ -507,8 +561,9 @@ if __name__ == "__main__":
             # get_statement(file, 'middle')  # statement_type= mini|middle|full|proctor
             # get_statement(file, 'full')  # statement_type= mini|middle|full|proctor
             # get_statement(file, 'proctor')  # statement_type= mini|middle|full|proctor
-            # get_proctor_report(file)
-            search_by_fio(file)     # Поиск учеток по фио
+            get_proctor_report(file)
+            # search_by_fio(file)     # Поиск учеток по фио
+    # create_requests("tm1.xlsx")
 
     # get_statement('РТФ_УИС_fall_2020.xlsx', statement_type='middle')  # Заказ конкретного отчета
     # get_statement('РТФ_УИС_fall_2020.xlsx', statement_type='full')  # Заказ конкретного отчета
--- grade_download.py
+++ grade_download.py
@@ -1,13 +1,15 @@
 from loguru import logger
 import datetime
 import grade_settings as GS
+import pandas as pnd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
-
+from datetime import date, timedelta
+import time
 
 def count_students(course_name: str, w_driver):
     """
@@ -287,10 +289,311 @@ def change_deadlines(course_name: str, email: str, deadline: str):
                 expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, '#set-extension > p:nth-child(7)')))
     driver.close()
 
+def change_scores(file):
+    df = pnd.read_excel('./scores/Book1.xlsx', sheet_name=0)
+    df = df.sort_values(by='course')
+    print(df)
+    driver = make_web_driver()
+    login(driver)
+
+    for i in range(len(df) - 1):
+        score = df.at[i, 'scores']
+        logname = df.at[i, 'logname']
+        block = df.at[i, 'block']
+        course_name = df.at[i, 'course']
+        print(score, logname, block, course_name)
+        course_url = f'https://courses.openedu.ru/courses/course-v1:urfu+{course_name}/instructor#view-student_admin'
+        driver.get(course_url)
+        driver.set_window_size(1920, 1022)
+
+        WebDriverWait(driver, 2000).until(
+            expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, '#set-extension > p:nth-child(7)')))
+        driver.find_element(By.NAME, "score-select-single").send_keys(score)
+        driver.find_element(By.NAME, "student-select-grade").send_keys(logname)
+        driver.find_element(By.NAME, "problem-select-single").send_keys(block)
+        driver.find_element(By.NAME, "override-problem-score-single").click()
+
+    driver.close()
+
+def date_open(txt: str):
+    i_month, i_day, i_year = txt.split('/')
+    p = date(int(i_year), int(i_month), int(i_day)) - timedelta(days=161)
+    return p.strftime('%m/%d/%Y')
+
+
+def datetostr(txt: str):
+    i_month, i_day, i_year = txt.split('/')
+    p = date(int(i_year), int(i_month), int(i_day))
+    return p.strftime('%d.%m.%Y')
+
+
+def todate(txt: str):
+    i_month, i_day, i_year = txt.split('/')
+    p = date(int(i_year), int(i_month), int(i_day))
+    return p
+
+def get_struct(course_name: str):
+    driver = make_web_driver()
+    login(driver)
+    driver.set_window_size(1920, 800)
+    course_url = f'https://studio.openedu.ru/course/course-v1:urfu+{course_name}'
+    driver.get(course_url)
+    WebDriverWait(driver, 30).until(expected_conditions.invisibility_of_element((By.CLASS_NAME, 'ui-loading')))
+
+    # Находим все элементы разделов и меняем им дату открытия
+    section_list = driver.find_elements_by_class_name('outline-section')
+    ids_sections = [subsection.get_attribute('Id') for subsection in section_list]
+
+    logger.info('Разделы')
+
+    for idd in ids_sections:
+        driver.implicitly_wait(10)
+
+        # открытие настроек раздела
+        driver.find_element_by_id(idd).find_element_by_class_name('configure-button').click()
+
+        # увеличение даты на определенное кол-во дней - для постепенного открытия материалов
+
+        # проверка даты открытия на случай сбоя - может часть уже поменяли и трогать дату не надо
+        if todate(driver.find_element_by_xpath('//*[ @ id = "start_date"]').get_attribute('value')) >= todate(
+                '09/06/2021'):
+            # print(datetostr(driver.find_element_by_xpath('//*[ @ id = "start_date"]').get_attribute(
+            #     'value')) + ";" + driver.find_element_by_xpath('//*[ @ id="modal-window-title"]').text)
+            # print("<tr><td>" + datetostr(driver.find_element_by_xpath('//*[ @ id = "start_date"]').get_attribute(
+            #     'value')) + "</td><td>" + driver.find_element_by_xpath('//*[ @ id="modal-window-title"]').text + "</td></tr>")
+
+            # лог для быстрой правки html графика открытия материалов
+            print("<tr><th colspan=2>" + driver.find_element_by_xpath(
+                '//*[ @ id="modal-window-title"]').text + "</th></tr>")
+            driver.find_element_by_css_selector('.action-cancel').click()
+        else:
+            new_date = date_open(driver.find_element_by_xpath('//*[ @ id = "start_date"]').get_attribute('value'))
+            # print(datetostr(new_date) + ";" + driver.find_element_by_xpath('//*[ @ id="modal-window-title"]').text)
+            print("<tr><th colspan=2>" + driver.find_element_by_xpath(
+                '//*[ @ id="modal-window-title"]').text + "</th></tr>")
+            # print("<tr><td>" + datetostr(new_date) + '</td><td>' + driver.find_element_by_xpath('//*[ @ id="modal-window-title"]').text + "</td></tr>")
+            driver.find_element_by_xpath('//*[ @ id = "start_date"]').clear()
+            driver.find_element_by_xpath('//*[ @ id = "start_date"]').send_keys(new_date)
+            driver.find_element_by_xpath('//*[ @ id = "start_time"]').clear()
+            driver.find_element_by_xpath('//*[ @ id = "start_time"]').send_keys('00:00')
+            driver.find_element_by_xpath('//*[ @ id = "start_time"]').click()
+            driver.find_element_by_class_name('modal-section-title').click()
+            driver.find_element_by_css_selector('.action-save').click()
+        time.sleep(2)  # ожидание 2 секунды, чтобы прогрузилось
+
+        #  ИЛИ   открыть все сразу
+        # driver.find_element_by_xpath('//*[ @ id = "start_date"]').clear()
+        # driver.find_element_by_xpath('//*[ @ id = "start_date"]').send_keys('30/06/2021')
+        # driver.find_element_by_xpath('//*[ @ id = "start_time"]').clear()
+        # driver.find_element_by_xpath('//*[ @ id = "start_time"]').send_keys('23:00')
+        # # driver.find_element_by_xpath('//*[ @ id = "start_time"]').click()
+        # driver.find_element_by_class_name('modal-section-title').click()
+        # driver.find_element_by_css_selector('.action-save').click()
+        # time.sleep(2)
+
+    # Находим все элементы подразделов и меняем им дату открытия. Заодно раскрываем списки
+    # driver.refresh()
+    subsection_list = driver.find_elements_by_class_name('outline-subsection')
+    ids_subsections = [subsection.get_attribute('Id') for subsection in subsection_list]
+
+    logger.info('Подразделы')
+
+    for idd in ids_subsections:
+        driver.implicitly_wait(10)
+        driver.find_element_by_id(idd).find_element_by_class_name('configure-button').click()
+
+        # постепенное открытие материалов по графику с проверкой сбоя скрипта - измененные даты не трогает
+        if todate(driver.find_element_by_xpath('//*[ @ id = "start_date"]').get_attribute('value')) >= todate(
+                '09/06/2021'):
+            print("<tr><td>" + datetostr(driver.find_element_by_xpath('//*[ @ id = "start_date"]').get_attribute(
+                'value')) + "</td><td>" + driver.find_element_by_xpath(
+                '//*[ @ id="modal-window-title"]').text + "</td></tr>")
+            driver.find_element_by_css_selector('.action-cancel').click()
+        else:
+            new_date = date_open(driver.find_element_by_xpath('//*[ @ id = "start_date"]').get_attribute('value'))
+            print("<tr><td>" + datetostr(new_date) + "</td><td>" + driver.find_element_by_xpath(
+                '//*[ @ id="modal-window-title"]').text + "</td></tr>")
+            driver.find_element_by_xpath('//*[ @ id = "start_date"]').clear()
+            driver.find_element_by_xpath('//*[ @ id = "start_date"]').send_keys(new_date)
+            driver.find_element_by_xpath('//*[ @ id = "start_time"]').clear()
+            driver.find_element_by_xpath('//*[ @ id = "start_time"]').send_keys('00:00')
+            driver.find_element_by_class_name('modal-section-title').click()
+            time.sleep(1)
+
+            # сброс всех дат на дефолтную дату открытия курса
+            # driver.find_element_by_xpath('//*[ @ id = "due_date"]').clear()
+            # driver.find_element_by_xpath('//*[ @ id = "due_time"]').clear()
+            # driver.find_element_by_class_name('modal-section-title').click()
+            # time.sleep(1)
+            # driver.find_element_by_css_selector('.action-save').click()
+
+        # ИЛИ открыть все сразу
+        # driver.find_element_by_xpath('//*[ @ id = "start_date"]').clear()
+        # driver.find_element_by_xpath('//*[ @ id = "start_date"]').send_keys('09/06/2021')
+        # driver.find_element_by_xpath('//*[ @ id = "start_time"]').clear()
+        # driver.find_element_by_xpath('//*[ @ id = "start_time"]').send_keys('00:00')
+        # driver.find_element_by_class_name('modal-section-title').click()
+        # time.sleep(1)
+        # driver.find_element_by_css_selector('.action-save').click()
+        # time.sleep(1)
+
+        # открытие содержимого подраздела - для изменения дедлайнов заданий
+        driver.find_element_by_id(idd).find_element_by_class_name('fa-caret-down').click()
+        time.sleep(1)
+
+    # Находим все элементы тем курса и меняем им дату дедлайна.
+    # driver.refresh()
+    unit_list = driver.find_elements_by_class_name('outline-unit')
+    ids_units = [unit.get_attribute('id') for unit in unit_list]
+
+    for idd in ids_units:
+        if len(driver.find_element_by_id(idd).find_elements_by_class_name('status-grading-label')) > 0:
+            # driver.implicitly_wait(10)
+            driver.find_element_by_id(idd).find_element_by_class_name('configure-button').click()
+            driver.find_element_by_xpath('//*[ @ id = "due_date"]').clear()
+            driver.find_element_by_xpath('//*[ @ id = "due_date"]').send_keys('01/10/2022')
+            driver.find_element_by_xpath('//*[ @ id = "due_time"]').clear()
+            driver.find_element_by_xpath('//*[ @ id = "due_time"]').send_keys('23:00')
+            driver.find_element_by_class_name('modal-section-title').click()
+            time.sleep(1)
+            driver.find_element_by_css_selector('.action-save').click()
+            time.sleep(2)
+        else:
+            continue
+
+    # Применяем обновления
+    for idd in ids_sections:
+        if len(driver.find_element_by_id(idd).find_elements_by_class_name('publish-button')) > 0:
+            # driver.implicitly_wait(10)
+            driver.find_element_by_id(idd).find_element_by_class_name('publish-button').click()
+            btn_name = driver.find_element_by_class_name('action-publish').text
+            btn_name = str.capitalize(btn_name)
+            driver.find_element_by_link_text(btn_name).click()
+            time.sleep(2)
+
+    driver.close()
+
+
+
+
+def change_score (course_name: str, score: str, logname: str, block):   # подставление баллов  по одному слушателю
+    driver = make_web_driver()
+    login(driver)
+    course_url = f'https://courses.openedu.ru/courses/course-v1:urfu+{course_name}/instructor#view-student_admin'
+    driver.get(course_url)
+    driver.set_window_size(1920, 1022)
+
+    driver.find_element(By.NAME, "score-select-single").send_keys(score)
+    driver.find_element(By.NAME, "student-select-grade").send_keys(logname)
+    driver.find_element(By.NAME, "problem-select-single").send_keys(block)
+    driver.find_element(By.NAME, "override-problem-score-single").click()
+    WebDriverWait(driver, 2000).until(
+        expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, '#set-extension > p:nth-child(7)')))
+    driver.close()
+
+
+
+def login_urfu(web_driver):
+    """
+    Функция принимает в качестве параметра настроенный WebDriver и проводит авторизацию на портале openedu.ru
+
+    :param web_driver:  WebDriver с которого ведется работа
+    """
+    # Авторизация на сайте openedu.ru
+    web_driver.get('https://courses.openedu.urfu.ru/login')
+    web_driver.set_window_size(1280, 720)
+    WebDriverWait(web_driver, 30000).until(expected_conditions.presence_of_element_located((By.ID, 'login-email')))
+    web_driver.find_element_by_id('login-email').send_keys(GS.USERNAME_URFU)
+    web_driver.find_element_by_id('login-password').send_keys(GS.PASSWORD_URFU)
+    web_driver.find_element_by_id('login-password').send_keys(Keys.ENTER)
+    # WebDriverWait(web_driver, 30000).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, 'username')))
+    # WebDriverWait(web_driver, 30000).until(expected_conditions.presence_of_element_located((By.XPATH, '//*[@id="navbarDropdown"]')))
+    WebDriverWait(web_driver, 30000).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, 'user-image-frame')))
+
+def grade_order_urfu(course_name: str, w_driver):
+    """
+    Функция переходит на страницу "Преподаватель", подраздел "Скачивание данных". Прокручивает страницу до
+    таблицы с выгрузками и ожидает ее загрузки. Затем нажимает на клавишу "Создать оценочный лист", ожидает появления
+    сообщения о том, что заказ принят или ошибку и переходит на главную страницу портала
+
+    :param course_name: Шифр курса (прим. ARCHC+fall_2020)
+    :param w_driver: WebDriver с которого ведется работа
+    """
+    print("grade_order_urfu")
+    course_url = f'https://courses.openedu.urfu.ru/courses/course-v1:UrFU+{course_name}/instructor#view-data_download'
+    w_driver.get(course_url)
+    w_driver.execute_script("window.scrollTo(0,1200)")
+    WebDriverWait(w_driver, 30000).until(expected_conditions.presence_of_element_located(
+        (By.CLASS_NAME, "file-download-link")))
+    w_driver.find_element_by_css_selector("input.async-report-btn:nth-child(1)").click()
+    logger.info('Заказан отчет по курсу: ' + course_name)
+    WebDriverWait(w_driver, 30000).until(lambda x: expected_conditions.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#report-request-response")) or expected_conditions.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#report-request-response-error")))  # Проверка двух условий работает только через lambda
+    w_driver.get('https://courses.openedu.urfu.ru/dashboard')
+
+def make_grade_report_order_urfu():
+    """
+    Функция создает WebDriver с настройками для заказа отчета Grade report, проходит по списку курсов и в каждом
+    из них нажимает на клавишу "Создать отчет" с помощью функции grade_order. Затем
+    завершает работу WebDriver.
+    """
+    print("make_grade_report_order_urfu")
+    driver = make_web_driver()
+    login_urfu(driver)
+    for course in GS.LIST_COURSES_URFU:
+        grade_order_urfu(course, driver)
+    driver.close()
+
+def make_exam_results_order_urfu():
+    """
+    Функция создает WebDriver с настрйоками для заказа отчета Grade report, проходит по списку курсов и в каждом
+    из них нажимает на клавишу "Создать отчет наблюдаемых испытаний" с помощью функции order_exam_results. Затем
+    завершает работу WebDriver.
+    """
+    driver = make_web_driver()
+    login_urfu(driver)
+    if driver.find_element_by_class_name('cc-dismiss'):
+        driver.find_element_by_class_name('cc-dismiss').click()
+    for course in GS.LIST_COURSES_URFU:
+        order_exam_results_urfu(course, driver)
+    driver.close()
+
+def order_exam_results_urfu(course_name: str, w_driver):
+    """
+    Функция переходит на страницу "Преподаватель", подраздел "Скачивание данных". Прокручивает страницу до
+    таблицы с выгрузками и ожидает ее загрузки. Затем нажимает на клавишу
+    "Создать отчёт о результатах наблюдаемого испытания", ожидает появления сообщения о том, что заказ принят
+    или ошибку и переходит на главную страницу портала.
+
+    :param course_name: Шифр курса (прим. ARCHC+fall_2020)
+    :param w_driver: WebDriver с которого ведется работа
+    """
+    course_url = f'https://courses.openedu.urfu.ru/courses/course-v1:UrFU+{course_name}/instructor#view-data_download'
+    w_driver.get(course_url)
+    w_driver.execute_script("window.scrollTo(0,1200)")
+    WebDriverWait(w_driver, 30000).until(expected_conditions.presence_of_element_located(
+        (By.CLASS_NAME, "file-download-link")))
+    w_driver.execute_script("window.scrollTo(0,500)")
+    try:
+        w_driver.find_element_by_css_selector(
+            ".reports-download-container > p:nth-child(10) > input:nth-child(1)").click()
+        WebDriverWait(w_driver, 30000).until(lambda x: expected_conditions.visibility_of_element_located(
+            (By.CSS_SELECTOR, "#report-request-response")) or expected_conditions.visibility_of_element_located(
+            (By.CSS_SELECTOR,
+                "#report-request-response-error")))  # Проверка двух условий работает только через lambda
+    except NoSuchElementException:
+        print("Нет наблюдаемых испытаний на курсе - " + course_name)
+    w_driver.get('https://courses.openedu.ru/')
 
 if __name__ == '__main__':
     # make_grade_report_order()   # Заказ отчета Grade Report
     # make_exam_results_order()   # Заказ отчета Exam Results
-    download_grade_report()     # Скачивание отчета Grade Report
+    make_grade_report_order_urfu()    # Заказ отчета с внутренней платформе Grade Report
+    # make_exam_results_order_urfu()  # Заказ отчета Exam Results с внутренней платформе
+    # download_grade_report()     # Скачивание отчета Grade Report
     # download_exam_results()     # Скачивание отчета Exam Results
+    # get_struct('ARCHC+fall_2021')
     # change_deadlines('PHILSCI+fall_2020', 'i.shaikhutdinova@mail.ru', '01/28/2021 23:30')
+    # change_score('TRIZ+spring_2021', '0.652', 'nikitamelnikov2', 'block-v1:urfu+TRIZ+spring_2021+type@problem+block@b665b9da521d4b80813aa9f7324aa094')  # Восстановление баллов
+    # change_scores('Book1.xlsx')  # Восстановление баллов
